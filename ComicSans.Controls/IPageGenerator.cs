b233280bc293e824b7c55e8d98e15527b66a222f
Jacob Ebey <jacob.ebey@live.com>
2015-08-28 11:43:30 -0700

    Added nuget things.

---
+++ ComicSans.Controls/IPageGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ComicSans.Controls
+{
+    /// <summary>
+    /// Interface defining methods for determining if and how a new <see cref="Xamarin.Forms.Page"/> should be
+    /// generated for the provided item.
+    /// </summary>
+    public interface IPageGenerator
+    {
+        /// <summary>
+        /// Determines if the item should have a new page created for it.
+        /// </summary>
+        /// <returns><c>true</c>, if a page should be generated for the item, <c>false</c> otherwise.</returns>
+        /// <param name="item">The item that is requesting to generate a new page.</param>
+        bool ShouldGenerateFrom(object item);
+
+        /// <summary>
+        /// Determines if the binding context for the newly created <see cref="Xamarin.Forms.Page"/>.
+        /// </summary>
+        /// <returns><c>true</c>, if set binding context was shoulded, <c>false</c> otherwise.</returns>
+        /// <param name="item">Item.</param>
+        bool ShouldSetBindingContextOfNewPage(object item);
+
+        /// <summary>
+        /// Generate a new page for the specified item.
+        /// </summary>
+        /// <param name="item">The item that the page is to be generated for.</param>
+        Page Generate(object item);
+    }
+}
+
---
+++ ComicSans.Controls/SubmenuListView.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ComicSans.Controls
+{
+    /// <summary>
+    /// A ListView that is capable of having submenus.
+    /// </summary>
+    public class SubmenuListView : ListView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ComicSans.Controls.SubmenuListView"/> class.
+        /// </summary>
+        public SubmenuListView()
+        {
+            ItemSelected += OnItemSelected;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="ComicSans.Controls.SubmenuListView"/> leaves the item selected when it is pressed.
+        /// </summary>
+        /// <value>Leaves items selected if <c>true</c>; otherwise unselects the item.</value>
+        public bool LeaveSelected
+        {
+            get { return (bool)GetValue(LeaveSelectedProperty); }
+            set { SetValue(LeaveSelectedProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the page generator.
+        /// </summary>
+        /// <value>The page generator.</value>
+        public IPageGenerator PageGenerator
+        {
+            get { return (IPageGenerator)GetValue(PageGeneratorProperty); }
+            set { SetValue(PageGeneratorProperty, value); }
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (PageGenerator?.ShouldGenerateFrom(e.SelectedItem) == true)
+            {
+                Page newPage = PageGenerator.Generate(e.SelectedItem);
+
+                if (PageGenerator.ShouldSetBindingContextOfNewPage(e.SelectedItem))
+                    newPage.BindingContext = e.SelectedItem;
+
+                Navigation.PushAsync(newPage);
+            }
+
+            if (!LeaveSelected)
+                (sender as ListView).SelectedItem = null;
+        }
+
+        /// <summary>
+        /// Gets the leave selected property.
+        /// </summary>
+        /// <value>The leave selected property.</value>
+        public static BindableProperty LeaveSelectedProperty { get; } =
+            BindableProperty.Create<SubmenuListView, bool>(ctrl => ctrl.LeaveSelected,
+                defaultValue: false,
+                defaultBindingMode: BindingMode.TwoWay,
+                propertyChanging: (bindable, oldValue, newValue) => ((SubmenuListView)bindable).LeaveSelected = newValue);
+
+        /// <summary>
+        /// Gets the page generator property.
+        /// </summary>
+        /// <value>The page generator property.</value>
+        public static BindableProperty PageGeneratorProperty { get; } =
+            BindableProperty.Create<SubmenuListView, IPageGenerator>(ctrl => ctrl.PageGenerator,
+                defaultValue: null,
+                defaultBindingMode: BindingMode.TwoWay,
+                propertyChanging: (bindable, oldValue, newValue) => ((SubmenuListView)bindable).PageGenerator = newValue);
+    }
+}
+
